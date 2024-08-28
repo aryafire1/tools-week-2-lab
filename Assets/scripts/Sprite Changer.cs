0edd3b06ad86f4418dd12bf6a8a03240545174dd
aryafire1 <erthorsen876@gmail.com>
2024-08-28 15:05:38 -0400

    stupid fucking bitch is stupid

---
+++ Assets/scripts/Sprite Changer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SpriteChanger : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public int spriteType;
+
+    renderer = gameObject.AddComponent<SpriteRenderer>();
+    renderer.sprite(spriteType);
+}
